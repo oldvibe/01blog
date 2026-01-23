7da5a01eeb66dae11bb96fe0ce04fbb9ec2366fd
ynaitedd <135625795+oldvibe@users.noreply.github.com>
2026-01-23 01:51:12 +0100

    Change method to find comments by post ID

--- src/main/java/com/blog01/backend/comment/CommentRepository.java
+++ src/main/java/com/blog01/backend/comment/CommentRepository.java
@@ -1,11 +1,10 @@
 package com.blog01.backend.comment;
 
-import com.blog01.backend.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findByPost(Post post);
+    List<Comment> findAllByPostId(Long postId);
 }
